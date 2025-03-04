b37bcaf99e3e9161260638a48846a5bc6ca5e48e
Behruz <behruzabdurahimovofficial@gmail.com>
2025-03-04 08:21:55 +0500

    g

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // LocalAPI
-// export const API = `http://localhost:1123/api`;
+export const API = `http://localhost:1123/api`;
 
 // GlobalAPI
-// export const API = `https://tashmetro-server.onrender.com/api`;
-export const API = `https://mkundalik-server.onrender.com/api`;
+// export const API = `https://mkundalik-server.onrender.com/api`;
