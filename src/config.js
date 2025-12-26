601b30142a00c4871b718d82f1b0f9bc92a8f8d0
Behruz Abdurakhimov <106621713+behruzed@users.noreply.github.com>
2025-12-26 15:17:14 +0500

    Update API endpoint in config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 // export const API = `http://localhost:1123/api`;
 // export const API = `https://api.mkundalik.uz/api`;
 
-export const API = `https://tashmetro-server-test.onrender.com/api`;
+export const API = `https://mkundalik.uzmetro.uz/api`;
 
 // export const API = `https://tashmetro-server3.onrender.com/api`;
