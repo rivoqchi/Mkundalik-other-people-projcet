3814795e9654e518e11931b3e0b78de415c4f6f2
Behruz Abdurakhimov <your_email@gmail.com>
2026-01-06 08:37:06 +0500

    cors uchun

--- src/config.js
+++ src/config.js
@@ -1,7 +1,2 @@
-// export const API = `https://076frgfw-1123.euw.devtunnels.ms/api`;
 // export const API = `http://localhost:1123/api`;
-// export const API = `https://api.mkundalik.uz/api`;
-
-export const API = `https://mkundalik.uzmetro.uz/api`;
-
-// export const API = `https://tashmetro-server3.onrender.com/api`;
+export const API = `https://api.mkundalik.uz/api`;
